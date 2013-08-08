6cb8d709c568cbe0b4e2dce7c40a12220cf8b073
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2013-08-08 13:01:03 -0300

    fix (ALL) Not working with brython 130801

--- fabfile.py
+++ fabfile.py
@@ -15,20 +15,66 @@ Kuarup - Fabric deployment
 __author__  = "Carlo E. T. Oliveira (cetoli@yahoo.com.br) $Author: cetoli $"
 __version__ = "1.1 $Revision$"[10:-1]
 __date__    = "2013/03/03 $Date$"
-
+from BeautifulSoup import BeautifulSoup as soup
 from fabric.api import local, settings, cd, run, lcd
+from base64 import b64decode as b6d
+import mechanize as mcz
+from tempfile import mkdtemp
 KG_ORIGIN = '/home/carlo/Dropbox/Android/git/vitallino'
 KG_DEST = '/home/carlo/Dropbox/Public/labase/kwarwp'
 #KG_DEST = '/tmp/kwarwp'
 SOURCES = '*.py'
 KSOURCES = 'kuarup.py tchuk.py kuarupfest.py tkinter_factory.py'
 KG_IMAGES = '/home/carlo/Dropbox/Android/git/vitallino/src/public/image'
-PARTS = '/src/kwarwp.html /src/*.py /src/public/image/*.* /brython.js /libs/*.js'.split()
-DESTS = '/src /src /src/public/image / /libs'.split()
+PARTS = '/src/kwarwp.html /src/dropmeme.html /src/*.py /src/public/image/*.* /brython.js /libs/*.js'.split()
+DESTS = '/src /src /src /src/public/image / /libs'.split()
 def hello():
     print("Hello world!")
-
-
+PLAT = 'https://activufrj.nce.ufrj.br/'
+PLAT = 'http://localhost:8888/'
+def __actdep(paswd):
+    _k_copy()
+def __actinit(mech):
+    mech.open(PLAT)
+    
+    mech.select_form(nr=0)
+    mech["user"] = "carlo"
+    mech["passwd"] = b6d(paswd)
+    results = mech.submit().read()
+    soup(results)
+    print (PLAT+'file/memit/')
+def __actup(mech, filename, folder = 'file/%smemit', orig = '/src/', single = None):
+    avs = mech.open(PLAT+folder%'').read()
+    #filename = 'cavalier.py'
+    if filename in avs:
+        mech.open(PLAT+folder%'delete/'+ '/' + filename).read()
+    avs = mech.open(PLAT+folder%'').read()
+    mech.select_form(nr=0)
+    mech.add_file(open(KG_ORIGIN + src + single or filename), 'text/plain', filename)
+    results = mech.submit().read()
+def actdep(paswd):
+    mech = mcz.Browser()
+    __actinit(mech)
+    for finename in 'meme.html memit.py'.split():
+        __actup(mech, filename)
+def actfig(paswd):
+    mech = mcz.Browser()
+    __actinit(mech)
+    PHASES = 7
+    PIECES = 9
+    #self.faces = ['valor.png','beleza.png','conforto.png'] * PHASES
+    backs = (['back%02d.jpg'%(phase)
+        for phase in range(PHASES)], 'soil.jpg')
+    puzzle = (['puzzle%02d.jpg'%(phase)
+        for phase in range(PHASES)], 'soil.jpg')
+    jigs = (['jigs%02d_%02d.jpg'%(phase,face) for face in range(PIECES)
+        for phase in range(PHASES)], 'soil.jpg')
+    faces = (['face%02d_%02d.jpg'%(phase,face) for face in [0,1,2]
+        for phase in range(PHASES)], 'soil.jpg')
+    pieces = (['piece%02d_%02d.png'%(kind,piece)
+        for piece in range(PIECES) for kind in [0,1]], 'soil.jpg')
+    for finename in faces:
+        __actup(mech, filename, folder = 'studio/%smemit', orig = '/src/public/image/')
 def ktest():
     local("nosetests")
 
